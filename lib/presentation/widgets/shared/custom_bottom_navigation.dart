af131f6e3200f582ae838b3649de03a702919b92
Jose Limbert <85981548+jlimbert1999@users.noreply.github.com>
2024-01-29 13:37:14 -0400

    configuracion de rutas para mantener estado

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustonBottomNavigator extends StatelessWidget {
-  const CustonBottomNavigator({super.key});
+  final int currentIndex;
+  const CustonBottomNavigator({super.key, required this.currentIndex});
+
+  void onItemTapped(BuildContext context, int index) {
+    context.go('/home/$index');
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: currentIndex,
+      onTap: (value) => onItemTapped(context, value),
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
         BottomNavigationBarItem(
